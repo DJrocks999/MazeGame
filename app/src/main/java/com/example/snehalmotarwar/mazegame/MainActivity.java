5d6fbed2572d72be8795b560838ab7e330a4a512
snehalsm <snehalmotarwar@gmail.com>
2015-07-26 16:39:17 +0530

    second commit

--- app/src/main/java/com/example/snehalmotarwar/mazegame/MainActivity.java
+++ app/src/main/java/com/example/snehalmotarwar/mazegame/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.snehalmotarwar.mazegame;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playButton = (Button) findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StagesActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
